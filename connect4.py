63e873aab11934fc85ee388ad7b07ea710de57ed
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-04-27 06:30:30 +0200

    Player switch implemented

--- connect4.py
+++ connect4.py
@@ -17,17 +17,26 @@ def main():
              [None,None,None,None,None,None,None],
              ]
     
-    active_player = 0
+    active_player = 1
     players = ["First","Second"]
     symbols = ["X", "O"]
     intro()
     print_board(board)
+    while endgame():
+        active_player = abs(active_player -1)
+        col = int(input(f"{players[active_player]} player, it's your turn. Choose a column: "))
+        process_choice(col)
 
 
-    
 
 
-def print_board(board):
+def process_choice(col):
+    pass
+    #CONTINUE HERE
+
+
+
+def print_board(board): 
     print()
     print("  1   2   3   4   5   6   7")
     print()
@@ -37,6 +46,20 @@ def print_board(board):
             print(cell if cell is not None else "_", end = " | ")
         print()
     print()
+
+def placement(board):
+    pass
+
+def intro():
+    print("------------------------")
+    print("Welcome to Connect4 Game")
+    print("------------------------")
+   
+
+
+def endgame():
+    return False
+    
 
 
 def placement(board):
@@ -49,6 +72,14 @@ def intro():
    
 
 
+def endgame():
+    return True
+
+
+    
+
 
 if __name__ == "__main__":
     main()
+
+
